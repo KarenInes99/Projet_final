33716e734b7169b3be0a8c161f15a006577f90de
KarenInes99 <karenouattara2@gmail.com>
2024-02-07 15:02:10 +0000

    update

--- auto/src/Components/Navigation.js
+++ auto/src/Components/Navigation.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Navbar,Button } from 'react-bootstrap';
 import {FaSignOutAlt} from 'react-icons/fa';
 
 
 const handleLogout = () => {
-  // Ajoutez ici la logique pour la déconnexion
   console.log('Déconnexion');
 };
 function Navigation() {
